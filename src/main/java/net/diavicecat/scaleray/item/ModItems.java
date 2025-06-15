7e78a416b1174e160d09d71682d0962832887ec1
Diavice <120302296+Diavice@users.noreply.github.com>
2025-06-15 12:45:31 +0100

    moditem

--- src/main/java/net/diavicecat/scaleray/item/ModItems.java
+++ src/main/java/net/diavicecat/scaleray/item/ModItems.java
@@ -1,4 +1,22 @@
 package net.diavicecat.scaleray.item;
 
+import net.diavicecat.scaleray.ScaleRays;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ScaleRays.MOD_ID);
+
+    public static final DeferredItem<Item> SCALERAY = ITEMS.register("scaleray",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> SCALINGCORE = ITEMS.register("scalingcore",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
